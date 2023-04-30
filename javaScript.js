02121592744ff00865873690f1a1d3ff2416b813
Ali Ostayoo <102816062+Osta-Code@users.noreply.github.com>
2023-04-30 13:14:27 +0500

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,7 @@
+function GenerateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src =  "http://thecatapi.com/api/images/get?format=src&type=gif";
+    div.appendChild(image);
+    
+}
